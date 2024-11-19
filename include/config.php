ca8e80cbed4af2fb53f7deeccbd54c15eda6ab07
saipulagung <80599373+saipulagung@users.noreply.github.com>
2024-11-19 09:19:30 +0700

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,8 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<mikhmon','mikhmon>|>aWNlbA==');
+
+$data['RB2011UiAS'] = array ('1'=>'RB2011UiAS!192.168.10.1','RB2011UiAS@|@admin','RB2011UiAS#|#oZ+Wp5+Xq5qTamg=','RB2011UiAS%@Sje.Biz','RB2011UiAS^sje.biz','RB2011UiAS&Rp','RB2011UiAS*10','RB2011UiAS(1','RB2011UiAS)','RB2011UiAS=10','RB2011UiAS@!@disable');
+$data['rb750r2'] = array ('1'=>'rb750r2!172.27.1.1:28728','rb750r2@|@admin','rb750r2#|#oZ+Wp5+Xq5qTamg=','rb750r2%@sje.biz','rb750r2^sje.biz','rb750r2&Rp','rb750r2*10','rb750r2(1','rb750r2)','rb750r2=10','rb750r2@!@disable');
+$data['RB433AH'] = array ('1'=>'RB433AH!192.168.200.127','RB433AH@|@admin','RB433AH#|#oZ+Wp5+Xq5qTamg=','RB433AH%@Sje.Biz','RB433AH^sje.biz','RB433AH&Rp','RB433AH*10','RB433AH(1','RB433AH)','RB433AH=10','RB433AH@!@disable');
+$data['lenovox86'] = array ('1'=>'lenovox86!id-27.hostddns.us:13673','lenovox86@|@admin','lenovox86#|#oZ+Wp5+Xq5qTamg=','lenovox86%WIFIKU','lenovox86^sje.biz','lenovox86&Rp','lenovox86*10','lenovox86(1','lenovox86)','lenovox86=10','lenovox86@!@disable');
